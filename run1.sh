4097996c5fa2e370168281952b64de08152f9f22
Mark Moorcroft <mntbighker@gmail.com>
2023-03-01 13:17:31 -0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -8,5 +8,4 @@ tar xf MacPorts-2.8.1.tar.bz2
 cd MacPorts-2.8.1
 ./configure --prefix=$HOME/local --with-install-user=$USER --with-install-group=staff --without-startupitems
 make && make install
-
 say "Mission accomplished"
