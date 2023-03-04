0d81badf60205899c6290b09846f75bc50354b85
Mark Moorcroft <mntbighker@gmail.com>
2023-03-03 18:47:15 -0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 
-open https://github.com/emilyst/home
+echo "Opening two web pages related to this setup\n"
+open "https://github.com/emilyst/home"
+open "https://www.youtube.com/@typecraft_dev"
 
 if ! [ -f $HOME/Mac_env/.tmux.conf ]; then
   echo "Please git clone into $HOME before running run2.sh\n"
   exit
 fi
 
+echo "Building and installing some local binaries\n"
 cd $HOME
 curl -O https://distfiles.macports.org/MacPorts/MacPorts-2.8.1.tar.bz2
 tar xf MacPorts-2.8.1.tar.bz2
@@ -16,4 +19,4 @@ make && make install
 cd $HOME
 rm -rf MacPorts-2.8.1 && rm MacPorts-2.8.1.tar.bz2
 
-say "Mission accomplished"
+say -v Rishi "Mission accomplished, well done"
