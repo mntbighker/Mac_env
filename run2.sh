294c274da2e9ca8e5ca9a9fdfde11230b6fc3e14
Mark Moorcroft <mntbighker@gmail.com>
2024-05-21 12:24:07 -0700

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -5,16 +5,9 @@ if ! [ -f $HOME/Mac_env/.tmux.conf ]; then
   exit
 fi
 
-export PATH=$HOME/local/bin:$PATH
-port selfupdate && port upgrade outdated
+brew update
 # port -N install autoconf automake bison bison-runtime boost176 brotli bzip2 cargo cmake cmake-bootstrap curl curl-ca-bundle docbook-xml docbook-xsl-nons expat gettext gettext-runtime gettext-tools-libs gmp gperf icu libarchive libb2 libcxx libedit libevent libffi libgit2 libiconv libidn2 libpsl libssh2 libtermkey libtextstyle libtool libunistring libutf8proc libuv libvterm libxml2 lua51 lua51-lpeg lua51-luarocks lua51-mpack luajit luarocks_select luv-luajit lz4 lzma lzo2 m4 msgpack msgpack-c msgpack-cpp ncurses neovim nghttp2 ninja nodejs19 npm9 openssl openssl3 p7zip pcre2 pkgconfig python3_select python311 python_select re2c readline rlwrap rust sqlite3 tmux tmux-pasteboard tree-sitter-cli unibilium wget xmlcatmgr xz zlib zstd
-port -N install neovim wget
-# port -f activate python311
-# port -f activate lua51-luarocks
-# port select --set python python311
-# port select --set python3 python311
-# port select --set pygments py311-pygments
-# port select --set luarocks lua51-luarocks
+brew install npm neovim wget
 
 echo "Downloading fancy bitmap fonts\n"
 cd ~/Library/Fonts && {
@@ -32,8 +25,8 @@ mv Mac_env/.config .
 mv Mac_env/.tmux.conf .
 
 echo 
-wget 'https://iterm2.com/downloads/stable/iTerm2-3_4_19.zip'
-unzip iTerm2-3_4_19.zip
+wget 'https://iterm2.com/downloads/stable/iTerm2-3_5_0.zip'
+unzip iTerm2-3_5_0.zip
 if ! [ -d $HOME/Applications ]; then
   mkdir $HOME/Applications
 fi
