0188d423f29f878711d89ef7f2801d7f9da9b7e8
Mark Moorcroft <mntbighker@gmail.com>
2024-05-20 18:56:43 -0700

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -7,15 +7,14 @@ fi
 
 export PATH=$HOME/local/bin:$PATH
 port selfupdate && port upgrade outdated
-port -N install autoconf automake bison bison-runtime boost176 brotli bzip2 cargo cmake cmake-bootstrap curl curl-ca-bundle docbook-xml docbook-xsl-nons expat gettext gettext-runtime gettext-tools-libs gmp gperf icu libarchive libb2 libcxx libedit libevent libffi libgit2 libiconv libidn2 libpsl libssh2 libtermkey libtextstyle libtool libunistring libutf8proc libuv libvterm libxml2 lua51 lua51-lpeg lua51-luarocks lua51-mpack luajit luarocks_select luv-luajit lz4 lzma lzo2 m4 msgpack msgpack-c msgpack-cpp ncurses neovim nghttp2 ninja nodejs19 npm9 openssl openssl3 p7zip pcre2 pkgconfig python3_select python311 python_select re2c readline rlwrap ruby27 ruby32 rust sqlite3 tmux tmux-pasteboard tree-sitter-cli unibilium wget xmlcatmgr xz zlib zstd
-port -f activate python311
-port -f activate ruby32
-port -f activate lua51-luarocks
-port select --set python python311
-port select --set python3 python311
-port select --set pygments py311-pygments
-port select --set luarocks lua51-luarocks
-port select --set ruby ruby32
+# port -N install autoconf automake bison bison-runtime boost176 brotli bzip2 cargo cmake cmake-bootstrap curl curl-ca-bundle docbook-xml docbook-xsl-nons expat gettext gettext-runtime gettext-tools-libs gmp gperf icu libarchive libb2 libcxx libedit libevent libffi libgit2 libiconv libidn2 libpsl libssh2 libtermkey libtextstyle libtool libunistring libutf8proc libuv libvterm libxml2 lua51 lua51-lpeg lua51-luarocks lua51-mpack luajit luarocks_select luv-luajit lz4 lzma lzo2 m4 msgpack msgpack-c msgpack-cpp ncurses neovim nghttp2 ninja nodejs19 npm9 openssl openssl3 p7zip pcre2 pkgconfig python3_select python311 python_select re2c readline rlwrap rust sqlite3 tmux tmux-pasteboard tree-sitter-cli unibilium wget xmlcatmgr xz zlib zstd
+port -N install neovim wget
+# port -f activate python311
+# port -f activate lua51-luarocks
+# port select --set python python311
+# port select --set python3 python311
+# port select --set pygments py311-pygments
+# port select --set luarocks lua51-luarocks
 
 echo "Downloading fancy bitmap fonts\n"
 cd ~/Library/Fonts && {
@@ -52,9 +51,6 @@ rm ~/.zshrc
 mv Mac_env/.zshrc .
 rm -rf ~/Mac_env
 
-echo "Installing ruby colorls gem\n"
-gem install colorls
-echo "You can add 'alias ls=colorls' to .zshrc to get the fancy one every time\n"
 echo "The command to run in each iTerm profile: \n\nssh -t ssh_config_alias_name 'tmux -CC new -A -s main' \n"
 echo "Run 'p10k configure' to customize the shell prompt\n"
 
