90fcb4594a0416d45288544c19aea186d89a8284
Mark Moorcroft <mntbighker@gmail.com>
2025-08-30 17:43:09 -0700

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -5,9 +5,9 @@ if ! [ -f $HOME/Mac_env/.zshrc ]; then
   exit
 fi
 
-brew update
+brew update; brew upgrade
 # port -N install autoconf automake bison bison-runtime boost176 brotli bzip2 cargo cmake cmake-bootstrap curl curl-ca-bundle docbook-xml docbook-xsl-nons expat gettext gettext-runtime gettext-tools-libs gmp gperf icu libarchive libb2 libcxx libedit libevent libffi libgit2 libiconv libidn2 libpsl libssh2 libtermkey libtextstyle libtool libunistring libutf8proc libuv libvterm libxml2 lua51 lua51-lpeg lua51-luarocks lua51-mpack luajit luarocks_select luv-luajit lz4 lzma lzo2 m4 msgpack msgpack-c msgpack-cpp ncurses neovim nghttp2 ninja nodejs19 npm9 openssl openssl3 p7zip pcre2 pkgconfig python3_select python311 python_select re2c readline rlwrap rust sqlite3 tmux tmux-pasteboard tree-sitter-cli unibilium wget xmlcatmgr xz zlib zstd
-brew install npm neovim wget lazygit cmake efm-langserver luarocks
+brew install npm neovim wget lazygit cmake efm-langserver luarocks rust rust-analyzer golang fzf fd zoxide eza yazi ffmpeg sevenzip jq poppler ripgrep imagemagick font-symbols-only-nerd-font
 
 echo "Downloading fancy bitmap fonts\n"
 cd ~/Library/Fonts && {
@@ -25,8 +25,8 @@ git clone https://github.com/radleylewis/nvim.git
 cd $HOME
 
 echo 
-wget 'https://iterm2.com/downloads/stable/iTerm2-3_5_10.zip'
-unzip iTerm2-3_5_10.zip
+wget 'https://iterm2.com/downloads/stable/iTerm2-3_5_14.zip'
+unzip iTerm2-3_5_14.zip
 if ! [ -d $HOME/Applications ]; then
   mkdir $HOME/Applications
 fi
@@ -44,7 +44,6 @@ rm ~/.zshrc
 mv Mac_env/.zshrc .
 rm -rf ~/Mac_env
 
-brew install fzf
 cat << 'EOF' >> ~/.zshrc
 # ---- FZF -----
 # Set up fzf key bindings and fuzzy completion
@@ -52,7 +51,6 @@ eval "$(fzf --zsh)"
 EOF
 source ~/.zshrc
 
-brew install fd
 cat << 'EOF' >> ~/.zshrc
 
 # -- Use fd instead of fzf --
@@ -90,14 +88,12 @@ export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${
 EOF
 source ~/.zshrc
 
-brew install zoxide
 # https://github.com/nanotee/zoxide
 cat << 'EOF' >> ~/.zshrc
 
 eval "$(zoxide init zsh --cmd cd)"
 EOF
 
-brew install eza
 cat << 'EOF' >> $HOME/.zshrc
 
 alias ls='eza'
@@ -105,9 +101,6 @@ EOF
 
 echo "The command to run in each iTerm profile: \n\nssh -t ssh_config_alias_name 'tmux -CC new -A -s main' \n"
 echo "Run 'p10k configure' to customize the shell prompt\n"
-
-# https://github.com/sxyazi/yazi
-brew install yazi ffmpeg sevenzip jq poppler ripgrep imagemagick font-symbols-only-nerd-font
 
 cat << 'EOF' >> ~/.zshrc
 function y() {
