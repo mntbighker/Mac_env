4786164fa6ef957b984d099ccc79a18fd4074072
Mark Moorcroft <mntbighker@gmail.com>
2025-10-25 23:14:58 -0700

    Update run2.sh to install iterm2 and remove old commands

--- run2.sh
+++ run2.sh
@@ -6,8 +6,7 @@ if ! [ -f $HOME/Mac_env/.zshrc ]; then
 fi
 
 brew update; brew upgrade
-# port -N install autoconf automake bison bison-runtime boost176 brotli bzip2 cargo cmake cmake-bootstrap curl curl-ca-bundle docbook-xml docbook-xsl-nons expat gettext gettext-runtime gettext-tools-libs gmp gperf icu libarchive libb2 libcxx libedit libevent libffi libgit2 libiconv libidn2 libpsl libssh2 libtermkey libtextstyle libtool libunistring libutf8proc libuv libvterm libxml2 lua51 lua51-lpeg lua51-luarocks lua51-mpack luajit luarocks_select luv-luajit lz4 lzma lzo2 m4 msgpack msgpack-c msgpack-cpp ncurses neovim nghttp2 ninja nodejs19 npm9 openssl openssl3 p7zip pcre2 pkgconfig python3_select python311 python_select re2c readline rlwrap rust sqlite3 tmux tmux-pasteboard tree-sitter-cli unibilium wget xmlcatmgr xz zlib zstd
-brew install npm neovim wget lazygit cmake efm-langserver luarocks rust rust-analyzer golang fzf fd zoxide eza yazi ffmpeg sevenzip jq poppler ripgrep imagemagick font-symbols-only-nerd-font
+brew install npm neovim wget lazygit cmake efm-langserver luarocks rust rust-analyzer golang fzf fd zoxide eza yazi ffmpeg sevenzip jq poppler ripgrep imagemagick font-symbols-only-nerd-font iterm2
 
 echo "Downloading fancy bitmap fonts\n"
 cd ~/Library/Fonts && {
@@ -21,24 +20,8 @@ rm Meslo.zip
 
 cd $HOME
 cd ~/.config
-git clone https://github.com/mntbighker/nvim.git
-cat << 'EOF' > nvim/lua/plugins/suda-vim.lua
-return {
-	"lewis6991/gitsigns.nvim",
-}
-EOF
-cat << 'EOF' >> nvim/.gitignore
-lua/plugins/suda-vim.lua
-EOF
+git clone https://github.com/radleylewis/nvim.git
 cd $HOME
-
-echo 
-wget 'https://iterm2.com/downloads/stable/iTerm2-3_5_14.zip'
-unzip iTerm2-3_5_14.zip
-if ! [ -d $HOME/Applications ]; then
-  mkdir $HOME/Applications
-fi
-mv iTerm.app $HOME/Applications
 
 echo "### Type exit after the oh-my-zsh script finishes, to complete setup ###\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
